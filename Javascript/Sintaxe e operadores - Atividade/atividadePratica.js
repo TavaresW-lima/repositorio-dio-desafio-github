c89b898cb95cb1b0a19ad7bdbb37cd278b7268fb
William Tavares <tavareswilliamlima@gmail.com>
2022-01-19 22:28:02 -0300

    Atividade Prática Sintaxe e Operadores - Versão refinada

--- Javascript/Sintaxe e operadores - Atividade/atividadePratica.js
+++ Javascript/Sintaxe e operadores - Atividade/atividadePratica.js
@@ -1,6 +1,34 @@
 function processaNumeros(num1, num2) {
-    let resultado = `Os números ${num1} e ${num2} ${num1 != num2 ? 'não' : ''} são iguais. Sua soma é ${num1 + num2}, que é ${(num1 + num2) > 10 ? 'maior' : 'menor'} que 10 e ${(num1 + num2) > 20 ? 'maior' : 'menor'} que 20.`;
-    console.log(resultado);
+    return `Os números ${num1} e ${num2} ${isEqual(num1,num2)}. `
+            .concat(`Sua soma é ${getSum(num1, num2)}, `)
+            .concat(`que é ${greaterOrLesserThan(getSum(num1, num2), 10)} 10 `)
+            .concat(`e ${greaterOrLesserThan(getSum(num1, num2), 20)} 20.`);
 }
 
-processaNumeros(82,8);
+function isEqual(num1, num2) {
+    return num1 == num2 
+            ? 'são iguais'
+            : 'não são iguais';
+}
+
+function getSum(num1, num2) {
+    return num1 + num2;
+}
+
+function greaterOrLesserThan(num1, num2) {
+    if(num1 > num2) {
+        return 'maior que'; 
+    } else if(num1 < num2) {
+        return 'menor que'; 
+    } else {
+        return 'igual a';
+    }
+}
+
+//Testes
+console.log(processaNumeros(5,5));
+console.log(processaNumeros(12,5));
+console.log(processaNumeros(10,10));
+console.log(processaNumeros(5, 5));
+
+console.log(processaNumeros(Math.floor(Math.random()*10),Math.floor(Math.random()*20)));
