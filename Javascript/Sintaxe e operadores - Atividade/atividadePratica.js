d1063610796851c086142a3055193d5238e10b99
William Tavares <tavareswilliamlima@gmail.com>
2022-01-19 22:15:14 -0300

    Atividade prática Sintaxe e Operadores Javascript (over simple/reckless version)

--- Javascript/Sintaxe e operadores - Atividade/atividadePratica.js
+++ Javascript/Sintaxe e operadores - Atividade/atividadePratica.js
@@ -1,29 +1,6 @@
 function processaNumeros(num1, num2) {
-    let resultado = `Os números ${num1} e ${num2} `;
-    let soma = num1 + num2;
-
-    if(num1 == num2) {
-        resultado = resultado.concat(`são iguais. `);
-    } else {
-        resultado = resultado.concat(`não são iguais. `);
-    }
-
-    resultado = resultado.concat(`Sua soma é ${soma}, que é `);
-
-    if(soma < 10) {
-        resultado = resultado.concat(`menor que 10 e `);
-    } else {
-        resultado = resultado.concat(`maior que 10 e `);
-    }
-
-    if(soma < 20) {
-        resultado = resultado.concat(`menor que 20.`);
-    } else {
-        resultado = resultado.concat(`maior que 20.`);
-    }
-
+    let resultado = `Os números ${num1} e ${num2} ${num1 != num2 ? 'não' : ''} são iguais. Sua soma é ${num1 + num2}, que é ${(num1 + num2) > 10 ? 'maior' : 'menor'} que 10 e ${(num1 + num2) > 20 ? 'maior' : 'menor'} que 20.`;
     console.log(resultado);
-
 }
 
 processaNumeros(82,8);
